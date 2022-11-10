111653ac515ea47d4109c14b1bfc4e40367acd21
shubham1994s <shubhamkawle41@gamil.com>
2022-11-10 10:36:55 +0530

    comit by shubhanm

--- SwachhBharatAbhiyan.CMS/Controllers/HomeController.cs
+++ SwachhBharatAbhiyan.CMS/Controllers/HomeController.cs
@@ -31,7 +31,8 @@ namespace SwachhBharatAbhiyan.CMS.Controllers
 
 
         public ActionResult Index()
-             if (SessionHandler.Current.AppId != 0)
+        {
+            if (SessionHandler.Current.AppId != 0)
             {
                 Session["AppName"] = SessionHandler.Current.AppName;
                 ViewBag.lat = SessionHandler.Current.Latitude;
