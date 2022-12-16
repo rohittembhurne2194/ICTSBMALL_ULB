df3392f51148378709f803bbbb392ad4365f535e
shubham1994s <shubhamkawle41@gamil.com>
2022-12-16 13:29:14 +0530

    comit by shubham

--- SwachhBharatAbhiyan.CMS/Content/DatatableGrid/BCDumpTrip.js
+++ SwachhBharatAbhiyan.CMS/Content/DatatableGrid/BCDumpTrip.js
@@ -19,10 +19,10 @@
 
     var table = $("#demoGrid").DataTable({
 
-    
+
         "sDom": "ltipr",
         "order": [[0, "desc"]],
-     //   "processing": true, // for show progress bar
+        //   "processing": true, // for show progress bar
         "serverSide": true, // for process server side
         "filter": true, // this is for disable filter (search box)
         "orderMulti": false, // for disable multiple column at once
@@ -41,35 +41,35 @@
                 "searchable": false
             }
                 ,
-                {
-                    "targets": [1],
-                    "visible": false,
-                    "searchable": false
+            {
+                "targets": [1],
+                "visible": false,
+                "searchable": false
+            },
+
+            {
+                "targets": [13],
+
+                "visible": true,
+
+                "render": function (data, type, full, meta) {
+
+
+
+                    if (full["TStatus"] == "0") {
+                        return "<div class='circle' style='height: 20px;width: 20px;background-color: #f44336;border-radius: 50%;    vertical-align: middle;display: inline-flex;'></div> (Failed !)";
+                    }
+                    else if (full["TStatus"] == "1") {
+                        return "<div class='circle' style='height: 20px;width: 20px;background-color: #388e3c;border-radius: 50%;vertical-align: middle;display: inline-flex;'></div> (Success)";
+
+                    }
+                    else {
+                        return "<div class='circle' style='height: 20px;width: 20px;background-color: #0086c3;border-radius: 50%;vertical-align: middle;display: inline-flex;'></div> (In Process)";
+
+                    }
+
                 },
-
-                {
-                    "targets": [13],
-
-                    "visible": true,
-
-                    "render": function (data, type, full, meta) {
-
-                     
-
-                        if (full["TStatus"] == "0") {
-                            return "<div class='circle' style='height: 20px;width: 20px;background-color: #f44336;border-radius: 50%;    vertical-align: middle;display: inline-flex;'></div> (Failed !)";
-                        }
-                        else if (full["TStatus"] == "1") {
-                            return "<div class='circle' style='height: 20px;width: 20px;background-color: #388e3c;border-radius: 50%;vertical-align: middle;display: inline-flex;'></div> (Success)";
-
-                        }
-                        else {
-                            return "<div class='circle' style='height: 20px;width: 20px;background-color: #0086c3;border-radius: 50%;vertical-align: middle;display: inline-flex;'></div> (In Process)";
-
-                        }
-
-                    },
-                },
+            },
             ],
 
         "columns": [
@@ -78,7 +78,9 @@
             { "data": "startDateTime", "name": "startDateTime", "autoWidth": false },
             { "data": "endDateTime", "name": "endDateTime", "autoWidth": false },
             { "data": "dyId", "name": "dyId", "autoWidth": false },
-            { "data": "houseList", "name": "houseList", "autoWidth": false },
+              { "data": "houseList", "name": "houseList", "autoWidth": false },
+          
+            { "render": function (data, type, full, meta) { return '<input  class="btn btn-sm btn-primary filter-button-style btnhlist" type="button"  value="View HouseList" />'; } },
             { "data": "userName", "name": "userName", "autoWidth": false },
             { "data": "vehicleNumber", "name": "vehicleNumber","autoWidth": false },
             { "data": "tripNo", "name": "tripNo", "autoWidth": false },
@@ -89,7 +91,7 @@
                 "data": "bcTransId",
                 "render": function (data, type, row, meta) {
                     if (type === 'display') {
-                        data = '<a target="_blank" href="https://mumbai.polygonscan.com/tx/' + data + '">View Transaction</a>';
+                        data = '<a target="_blank" class="btn btn-sm btn-primary filter-button-style" href="https://mumbai.polygonscan.com/tx/' + data + '">View Transaction</a>';
                     }
 
                     return data;
@@ -99,7 +101,18 @@
             { "data": "TStatus", "name": "TStatus", "autoWidth": false },
         ]
     });
+    $('#demoGrid tbody').on('click', '.btnhlist', function () {
+        debugger;
+
+        var data = table.row($(this).parents('tr')).data();
+        $('#myModal_Image').modal('toggle');
+        jQuery("#dateData").text(data.houseList);
+        $('#myModal_Image').modal('show');
+    });
 });
+
+
+
 
 function ViewBT(bcTransId) {
     debugger;
